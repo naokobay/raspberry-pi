8ba068ff9523af936486e9525d728f26c73d254f
naokobay <mnaokobay@yahoo.co.jp>
2018-01-27 15:42:42 +0000

    update

--- LED/led.py
+++ LED/led.py
@@ -1,2 +1,11 @@
 import wiringpi
 import time
+led_pin = 23
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode( led_pin, 1 )
+while True:
+	wiringpi.digitalWrite( led_pin, 1 )
+	time.sleep(1)
+	wiringpi.digitalWrite( led_pin, 0 )
+	time.sleep(1)
+
