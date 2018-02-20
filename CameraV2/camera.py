426184307f918295c5f831a19608e6fd0e156092
naokobay <mnaokobay@yahoo.co.jp>
2018-02-21 01:25:09 +0900

    first commit

---
+++ CameraV2/camera.py
@@ -0,0 +1,17 @@
+import picamera
+import datetime
+import os
+ 
+now = datetime.datetime.now()
+dir_name = now.strftime('%Y%m%d')
+dir_path = '/home/pi/github/raspberry-pi/CameraV2/'+dir_name
+file_name = now.strftime('%H%M%S')
+ 
+if not os.path.exists(dir_path):
+	os.makedirs(dir_path)
+os.chmod(dir_path, 0777)
+ 
+picamera = picamera.PiCamera()
+picamera.resolution = (3280,2464)
+picamera.capture(dir_path+'/'+file_name+'.jpg')
+
